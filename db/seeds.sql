551526fb663c1eff1ce725ddee885b8de746b8a7
lizardskins <riley.barlow@gmail.com>
2018-07-30 09:04:58 -0600

    final Update

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burger_db;
+INSERT INTO burgers (burger_name) VALUES ('jalapeno burger');
+INSERT INTO burgers (burger_name) VALUES ('western bacon');
+INSERT INTO burgers (burger_name) VALUES ('cheese');
